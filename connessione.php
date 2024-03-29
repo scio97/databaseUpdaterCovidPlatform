c8f0dd86c705cee64fb4412d2f12776bef30dc42
Fabio Sciortino <scio97@hotmail.it>
2024-03-29 17:38:43 +0100

    aggiornamento dati database

--- connessione.php
+++ connessione.php
@@ -1,12 +1,11 @@
 <?php
-$servername = "remotemysql.com";
-$username = "MB8CKtLiWy";
-$password = "qYUGJJqFG5";
-$db="MB8CKtLiWy";
+$servername = "localhost";
+$username = "root";
+$password = "123456";
+$db="covidPlatform";
 try {
     $conn = new PDO("mysql:host=$servername;dbname=$db", $username, $password);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    echo "Connessione riuscita";
 }catch(PDOException $e){
     echo "Connessione fallita: " . $e->getMessage();
 }
--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php
+set_time_limit(0);
 include("funzioni.php");
 updateAndamento_nazionale();
 updateAndamento_regionale();
